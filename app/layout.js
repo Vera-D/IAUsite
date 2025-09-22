a0b4b989567bcea00aadf331fcf00c0eb53cd9c5
Vera-D <kltsklrs@gmail.com>
2025-09-23 02:14:14 +0300

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import Footer from "./components/Footer/Footer";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+
 
 export const metadata = {
 	title: "ITS ABOUT US",
@@ -15,6 +17,7 @@ export default function RootLayout({ children }) {
 			<body className="font-poppins font-rendering text-2xl xl:text-3xl">
 				{children}
 				 <Footer /> 
+				 <Analytics />
 			</body>
 		</html>
 	);
